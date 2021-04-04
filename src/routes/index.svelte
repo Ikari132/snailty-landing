845f68bb3507db01ac66d76e6c35b243fa4be466
Anton Aksionau <ikari808@gmail.com>
2021-04-04 21:38:15 +0200

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+</script>
+
+<main>
+	<h1>Hello on snailty</h1>
+	<h2>Your conductor to the world of tea</h2>
+</main>
+
+<style lang="scss">
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4rem;
+		font-weight: 100;
+		line-height: 1.1;
+		margin: 4rem auto;
+		max-width: 14rem;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			max-width: none;
+		}
+	}
+</style>
